33cb662e62f61c972f0670952ee066d6436340e8
01452 <102763331+01452@users.noreply.github.com>
2025-09-24 12:52:40 +0200

    added modelMapper

--- src/main/java/cohort_65/java/forumservice/post/service/PostServiceImpl.java
+++ src/main/java/cohort_65/java/forumservice/post/service/PostServiceImpl.java
@@ -6,6 +6,7 @@ import cohort_65.java.forumservice.post.dto.NewPostDto;
 import cohort_65.java.forumservice.post.dto.PostDto;
 import cohort_65.java.forumservice.post.model.Post;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,19 +16,13 @@ import java.util.List;
 public class PostServiceImpl implements PostService {
 
     final PostRepository postRepository;
+    final ModelMapper modelMapper;
 
     @Override
     public PostDto addNewPost(NewPostDto newPostDto, String author) {
         Post post = new Post(newPostDto.getTitle(),
                 newPostDto.getContent(), author, newPostDto.getTags());
         post = postRepository.save(post);
-        List<CommentDto> lists = post.getComments().stream().map(
-                c -> new CommentDto(c.getUser(),c.getMessage(),
-                        c.getLikes(),c.getDateCreated())
-        ).toList();
-        return new PostDto(post.getId(),post.getTitle(),
-                post.getContent(),post.getAuthor(),
-                post.getTags(),post.getLikes(),post.getDateCreated(),
-                lists);
+        return modelMapper.map(post, PostDto.class);
     }
 }
