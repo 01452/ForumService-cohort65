f662cca5e6af7de236759c73ae98fcca2f7173eb
01452 <102763331+01452@users.noreply.github.com>
2025-09-26 10:59:22 +0200

    added accounting module

--- src/main/java/cohort_65/java/forumservice/accounting/service/UserAccountServiceImpl.java
+++ src/main/java/cohort_65/java/forumservice/accounting/service/UserAccountServiceImpl.java
@@ -3,6 +3,7 @@ package cohort_65.java.forumservice.accounting.service;
 import cohort_65.java.forumservice.accounting.dao.UserAccountRepository;
 import cohort_65.java.forumservice.accounting.dto.UserDto;
 import cohort_65.java.forumservice.accounting.dto.UserRegisterDto;
+import cohort_65.java.forumservice.accounting.dto.UserUpdateDto;
 import cohort_65.java.forumservice.accounting.dto.exception.UserExistsException;
 import cohort_65.java.forumservice.accounting.dto.exception.UserNotFoundException;
 import cohort_65.java.forumservice.accounting.model.UserAccount;
@@ -31,6 +32,37 @@ public class UserAccountServiceImpl implements UserAccountService {
     public UserDto getUserByLogin(String login) {
         UserAccount userAccount = userAccountRepository.findById(login)
                 .orElseThrow(UserNotFoundException::new);
+        return modelMapper.map(userAccount, UserDto.class);
+    }
+
+    @Override
+    public UserDto removeUserByLogin(String login) {
+        UserAccount userAccount = userAccountRepository.findById(login)
+                .orElseThrow(UserNotFoundException::new);
+        userAccountRepository.delete(userAccount);
+        return modelMapper.map(userAccount, UserDto.class);
+    }
+
+    @Override
+    public UserDto updateUserByLogin(String login, UserUpdateDto userUpdateDto) {
+        UserAccount userAccount = userAccountRepository.findById(login)
+                .orElseThrow(UserNotFoundException::new);
+        if (userUpdateDto.getLastName() != null) {
+            userAccount.setLastName(userUpdateDto.getLastName());
+        }
+        if (userUpdateDto.getFirstName() != null) {
+            userAccount.setFirstName(userUpdateDto.getFirstName());
+        }
+        userAccount = userAccountRepository.save(userAccount);
+        return modelMapper.map(userAccount, UserDto.class);
+    }
+
+    @Override
+    public UserDto changeRoleForUser(String login, String role, boolean isAddRole) {
+        UserAccount userAccount = userAccountRepository.findById(login)
+                .orElseThrow(UserNotFoundException::new);
+        boolean res = isAddRole ? userAccount.addRole(role) : userAccount.removeRole(role);
+        if (res) userAccount = userAccountRepository.save(userAccount);
         return modelMapper.map(userAccount, UserDto.class);
     }
 }
