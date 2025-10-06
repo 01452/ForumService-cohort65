086182f2960cabe759a124d945e1c59958179b65
01452 <102763331+01452@users.noreply.github.com>
2025-10-06 12:54:25 +0200

    added spring security

---
+++ src/main/java/cohort_65/java/forumservice/security/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package cohort_65.java.forumservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/account/register").permitAll()
+                .requestMatchers(HttpMethod.DELETE,"/account/user/{login}")
+                    .access(new WebExpressionAuthorizationManager(
+                        "hasRole('ADMIN') or authentication.name == #login"))
+                .anyRequest().authenticated());
+        return http.build();
+    }
+}
